2b70ec492ce62670dcacd3eebd03c9f15e7867ca
Timofey Petukhov <smileedition@gmail.com>
2019-09-14 22:08:07 +0700

    added funcA

---
+++ funcA.cpp
@@ -0,0 +1,7 @@
+#include "funcA.h"
+#include <iostream>
+
+bool funcA( int x, int y )
+{
+     return x == y;
+}
---
+++ funcA.h
@@ -0,0 +1,10 @@
+//
+// Created by subeo on 9/11/19.
+//
+
+#ifndef PROJECTMAKE_FUNCA_H
+#define PROJECTMAKE_FUNCA_H
+
+bool funcA( int x, int y );
+
+#endif //PROJECTMAKE_FUNCA_H
