fe1a628777dff8b5fa725924edd073bf1aa33e62
Timofey Petukhov <smileedition@gmail.com>
2019-09-14 22:08:52 +0700

    added foo

---
+++ foo/foo.cpp
@@ -0,0 +1,10 @@
+//
+// Created by subeo on 9/13/19.
+//
+
+#include "foo.h"
+
+bool foo::nequ( int x, int y )
+{
+     return x != y;
+}
---
+++ foo/foo.h
@@ -0,0 +1,16 @@
+//
+// Created by subeo on 9/13/19.
+//
+
+#ifndef PROJECTMAKE_FOO_H
+#define PROJECTMAKE_FOO_H
+
+
+class foo
+{
+public:
+     static bool nequ( int x, int y);
+};
+
+
+#endif //PROJECTMAKE_FOO_H
